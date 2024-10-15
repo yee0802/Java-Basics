48562a31a27d648735d8da7a0aae09ab64fbe4d6
yee0802 <NovusAa12@proton.me>
2024-10-15 16:43:38 +0100

    feat: add favFilm variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,12 @@ public class Main {
         int birthYear = 2002;
         double pi = 3.14;
 
+        String favFilm = "Finding Nemo";
+        favFilm = "Hannah Montana: The Movie";
+        System.out.printf("My favourite film is %s%n", favFilm);
+
         boolean isLearningJava = true;
-        System.out.printf("My name is %s, and my birth year is %d %n", name, birthYear);
-        System.out.printf("Learning java? %b %n", isLearningJava);
+        System.out.printf("My name is %s, and my birth year is %d%n", name, birthYear);
+        System.out.printf("Learning Java? %b%n", isLearningJava);
     }
 }
