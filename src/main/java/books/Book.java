cfe1a4d7d70e4b72c323f6d679c08ce44302925a
yee0802 <NovusAa12@proton.me>
2024-10-15 17:06:15 +0100

    feat: add Book constructor

--- src/main/java/books/Book.java
+++ src/main/java/books/Book.java
@@ -3,4 +3,9 @@ package books;
 public class Book {
     String title;
     int pages;
+
+    public Book(String title, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
 }
