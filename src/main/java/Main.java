577717db4c0f668813add666a66439393c4f53ae
yee0802 <NovusAa12@proton.me>
2024-10-15 16:12:37 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Kye";
+        String name2 = "Kye";
+
+        String name3 = new String("Kye");
+        int birthYear = 2002;
+
+        System.out.printf("My name is %s, and my birth year is %d", name, birthYear);
+    }
+}
