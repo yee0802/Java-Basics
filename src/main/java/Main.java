cdf7e16eba19f180395a14fee44f33cd76162db4
yee0802 <NovusAa12@proton.me>
2024-10-15 16:20:28 +0100

    feat: add pi variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Kye";
-        String name2 = "Kye";
-
-        String name3 = new String("Kye");
         int birthYear = 2002;
+        double pi = 3.14;
 
         System.out.printf("My name is %s, and my birth year is %d", name, birthYear);
     }
